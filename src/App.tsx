f23191c95b506944846c2709b3de4af42a22b445
mirza <zanuarmirza@gmail.com>
2022-05-10 10:40:27 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="flex justify-center">
+      <h1 className="font-bold text-2xl text-blue-900">
+        React and Tailwind with Vitejs!
+      </h1>
+    </div>
+  );
+}
+
+export default App
